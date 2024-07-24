b15a56c592600b59eabb8fc60dff9e91edc6718c
Am0r123 <168203012+Am0r123@users.noreply.github.com>
2024-07-24 13:28:12 +0300

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject,OnInit } from '@angular/core';
 import { TasksComponent } from './tasks/tasks.component';
 import { FilterComponent } from './filter/filter.component';
+import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, '/assets/i18n/', '.json');
+}
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [TasksComponent,FilterComponent],
+  imports: [RouterOutlet,TasksComponent,FormsModule,CommonModule,FilterComponent,
+    HttpClientModule,
+    TranslateModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -12,5 +26,23 @@ export class AppComponent {
   selectedradiobtn : string ='all';
   onfilterchanged(value :string){
     this.selectedradiobtn = value;
+  }
+
+  languages = ['en', 'es','ar'];
+  private translate: TranslateService = inject(TranslateService);
+
+  ngOnInit() {
+    const defaultLanguage = 'en';
+    this.translate.setDefaultLang(defaultLanguage);
+    this.translate.use(defaultLanguage);
+  }
+
+  switch(lang: string='en') {
+    this.translate.use(lang);
+  }
+
+  onSelectionChange(event: Event) {
+    const target = event.target as HTMLSelectElement;
+    this.translate.use(target.value);
   }
 }
--- src/app/filter/filter.component.ts
+++ src/app/filter/filter.component.ts
@@ -2,10 +2,11 @@ import { Component, EventEmitter, inject, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { TaskService } from '../services/task.service';
+import { TranslateModule } from '@ngx-translate/core';
 @Component({
   selector: 'filter',
   standalone: true,
-  imports: [CommonModule,FormsModule],
+  imports: [CommonModule,FormsModule,TranslateModule],
   templateUrl: './filter.component.html',
   styleUrl: './filter.component.css'
 })
